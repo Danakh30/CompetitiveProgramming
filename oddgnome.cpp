3001e7a7581e144c5db90b734165a90f3e7c8bd3
danakh30 <danakh30@aucegypt.edu>
2024-05-09 13:51:23 +0300

    X

--- oddgnome.cpp
+++ oddgnome.cpp
@@ -5,13 +5,22 @@ int main()
 {
     int n, g;
     cin >> n;
-    int maxi = INT_MIN;
+    vector<int> gnomes;
     while (n--)
     {
-
+        cin >> g;
+        gnomes.resize(g);
         for (int i = 0; i < g; i++)
         {
-            cin >> g;
+            cin >> gnomes[i];
+        }
+        for (int i = 1; i < g - 1; i++)
+        {
+            if (gnomes[i] - gnomes[i - 1] != 1)
+            {
+                cout << i + 1;
+                break;
+            }
         }
     }
     return 0;
